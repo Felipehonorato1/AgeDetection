bbecbb4d2cf169606562b1a347f34b76b4659639
Felipe Honorato <felipesousa@eng.ci.ufpb.br>
2021-03-17 21:40:23 -0300

    Implementing class for crop and resize

--- face_detection/detector.py
+++ face_detection/detector.py
@@ -1,19 +1,31 @@
 import cv2
+import os
 # cap = cv2.VideoCapture()
 
-face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 
-# while True:
-#     _, img = cap.read()
-img = cv2.imread('ppl2.jpg')
+class img_processing():
+    def __init__(self, img_path, output_path):
+        self.face_cascade = cv2.CascadeClassifier(
+            'haarcascade_frontalface_default.xml')
+        self.img_path = img_path
+        self.output_path = output_path
 
-gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    def resize(self, img):
+        return cv2.resize(img, (200, 200), interpolation=cv2.INTER_AREA)
 
-i = 0
-faces = face_cascade.detectMultiScale(gray, 1.1, 4)
+    def crop_images(self):
+        for img in os.listdir(self.img_path):
+            img_read = cv2.imread(f'{self.img_path}/{img}')
 
-for (x, y, w, h) in faces:
-    #cv2.rectangle(img, (x, y), (x+w, y+h), (255, 0, 0), 2)
-    img_crop = img[y:y+h, x:x+w]
-    cv2.imwrite(f'crop{i}.jpg', img_crop)
-    i += 1
+            gray = cv2.cvtColor(img_read, cv2.COLOR_BGR2GRAY)
+
+            faces = self.face_cascade.detectMultiScale(gray, 1.1, 4)
+
+            for (x, y, w, h) in faces:
+                if (w > 200) & (h > 200):
+                    img_crop = img[y:y+h, x:x+w]
+                    img_resized = self.resize(img_crop)
+                    self.img_id += 1
+                    cv2.imwrite(f'{self.output_path}/{img}.jpg')
+
+        printf('DONE.')
